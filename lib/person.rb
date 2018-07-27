818b70147b0e9e1ffb2e0af623a799abb3fc4988
anna-rose <917566@isd624.org>
2018-07-27 19:20:55 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,17 @@
 # person.rb
+class Person 
+  def name=(dog_name)
+    @name = dog_name
+  end
+ 
+  def name
+    @name
+  end
+  def breed=(breed)
+    @breed = breed
+  end
+ 
+  def breed
+    @breed
+  end
+end
